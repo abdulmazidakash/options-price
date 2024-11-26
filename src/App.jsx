772b6942da75cf74e011ffcf363f7e60b1a719b8
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-26 21:55:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <p className="text-4xl bg-rose-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
